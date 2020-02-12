1aa4bdf4d8a5616c2dcbb299d338a048391a1df3
khadijah faison <khadijahfaison@gmail.com>
2020-02-11 19:22:12 -0800

    hard mode

--- main.js
+++ main.js
@@ -1,6 +1,51 @@
 $(document).ready(function() {
-  $("#add").on("click", function(e) {
+  const state = {
+    num1: "",
+    oper: "",
+    num2: ""
+  }
+
+  $("#calc").on("click", "button", function(e) {
     e.preventDefault()
-    var val = Number($("#num1").val())
+
+    var val = $(this).html()
+
+    if (val === "x" || val === "/" || val === "+" || val === "-") {
+      if (state.num2 !== "") {
+        operation()
+      }
+      state.oper = val
+    } else if (val === "=") {
+      operation()
+    } else if (val === "C") {
+      state.num1 = ""
+      state.oper = ""
+      state.num2 = ""
+    } else {
+      if (state.oper === "") {
+        state.num1 += val
+        $("#result").html(state.num1)
+      } else {
+        state.num2 += val
+        $("#result").html(state.num2)
+      }
+    }
   })
+
+  function operation() {
+    if (state.oper === "+") {
+      state.num1 = Number(state.num1) + Number(state.num2)
+    } else if (state.oper === "-") {
+      state.num1 = Number(state.num1) - Number(state.num2)
+    } else if (state.oper === "x") {
+      state.num1 = Number(state.num1) * Number(state.num2)
+    } else if (state.oper === "/") {
+      state.num1 = Number(state.num1) / Number(state.num2)
+    }
+
+    $("result").html(state.num1)
+
+    state.oper = ""
+    state.num2 = ""
+  }
 })
